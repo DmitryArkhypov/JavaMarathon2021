c9fd0c81df86ddde5e62d6feef10f337f0cc73c9
Dmitry Arkhipov <arhipov181@gmail.com>
2022-02-09 20:22:44 +0200

    complete days 1-5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
         Car car1 = new Car();
         car1.setModel("BMW");
         car1.setColor("red");
-        car1.setYear(2022);
+        car1.setYear(2020);
         System.out.println(car1.getModel());
         System.out.println(car1.getColor());
         System.out.println(car1.getYear());
